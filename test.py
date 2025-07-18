f9e9f49f12b143ec363e53b1142ebaafc077ac7f
Tom Mwambingu <[tomchom30@gmail.com]>
2025-07-18 12:13:56 +0300

    Added Dice function

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
 import tkinter as tk
 import random
+
+# Roll dice function
+def roll_dice():
+    dice = random.randint(1, 6)
+    current = player_turn.get()
+    positions[current] = min(positions[current] + dice, 99)
+    draw_tokens()
+    info_label.config(text=f"{current.capitalize()} rolled a {dice}")
+    next_turn()
